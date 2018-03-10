7ef1b897373201f8ea92a0ef0ae8b192d4f1f8fb
gresybustamante <gresybustamante@gresys-mbp.fios-router.home>
2018-03-10 12:23:05 -0500

    added print statement

--- src/Main/GoodEvening.java
+++ src/Main/GoodEvening.java
@@ -1,5 +1,9 @@
 package Main;
 
 public class GoodEvening {
+	public static void main(String[] args) {
+		System.out.println("Goof Evening Class!");
+		
+}
 
 }
--- src/Main/GoodMorning.java
+++ src/Main/GoodMorning.java
@@ -1,5 +1,9 @@
 package Main;
 
 public class GoodMorning {
+	public static void main(String[] args) {
+		System.out.println("Good Morning Class!");
+	
+}
 
 }
